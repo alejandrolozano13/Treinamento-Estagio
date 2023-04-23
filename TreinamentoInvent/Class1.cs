fc698f8b2fc2ddb5d9bfa5e9fc135d2f03810667
Alejandro <alejandro20181999@gmail.com>
2023-04-23 20:44:24 -0300

    primeiro estágio

---
+++ TreinamentoInvent/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinamentoInvent
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        public DateTime Data { get; set; }
+    }
+}
